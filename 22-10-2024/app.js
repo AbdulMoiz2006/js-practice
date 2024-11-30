e3374c95d194abff913023037019d0b9bc940dab
AbdulMoiz2006 <156921517+AbdulMoiz2006@users.noreply.github.com>
2024-11-30 19:49:49 +0500

    Add files via upload

---
+++ 22-10-2024/app.js
@@ -0,0 +1,51 @@
+// var cities = ["Karachi","Multan","Lahore","islamabad"]
+// cities[1] = 'peshawar'
+// cities[3] = "England"
+
+// console.log(cities);
+
+
+
+
+
+// var cities = ["Karachi","Multan","Lahore","islamabad","peshawar"]
+
+
+// console.log(cities.length);
+
+
+// var cities = ["Karachi","Multan","Lahore","islamabad"]
+
+
+// cities.push("quetta")
+
+// console.log(cities);
+
+
+// var cities = ["Karachi","Multan","Lahore","islamabad"]
+
+
+// cities.pop()
+
+// console.log(cities);
+
+
+// var cities = ["Karachi","Multan","Lahore","islamabad"]
+
+
+// cities.unshift("Faisalabad")
+
+
+// console.log(cities);
+
+
+
+// var cities = ["Karachi","Multan","Lahore","islamabad"]
+
+
+// cities.shift()
+
+
+// console.log(cities);
+
+
