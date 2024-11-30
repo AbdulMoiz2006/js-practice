54757552d69f315fc8d653435a5ed07075f4f93e
AbdulMoiz2006 <156921517+AbdulMoiz2006@users.noreply.github.com>
2024-11-30 19:42:34 +0500

    Add files via upload

---
+++ 15-11-2024/app.js
@@ -0,0 +1,69 @@
+// var username = prompt("Enter your Name")
+
+// alert(username)
+
+
+
+
+
+// var username = prompt("Enter your Name")
+
+
+
+// if(username ===""){
+
+// alert("You didnt enter your name!")
+
+// }
+// else{
+//     alert(username)
+// }
+
+
+// var username = prompt("Enter your name")
+
+// if(username === "Ali"){
+
+//     alert(`Hello,${username}!Welcome!`)
+
+// }
+// else if(username === ""){
+
+
+
+//  alert("You didnt enter your name!")
+
+// }
+
+
+
+// else{
+//     alert(`Hello,${username}!`)
+// }
+
+
+
+
+
+
+var username = prompt("Enter your name")
+
+if(username === "Ali"){
+
+    alert(`Hello,${username}!Welcome!`)
+    alert(`Hope you're having a great day,${username}!`)
+
+}
+else if(username === ""){
+
+
+
+ alert("You didnt enter your name!")
+
+}
+
+
+
+else{
+    alert(`Hello,${username}!`)
+}
