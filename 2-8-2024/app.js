e2e1e0af21aad83ea500a47f176f0c5809b67049
AbdulMoiz2006 <156921517+AbdulMoiz2006@users.noreply.github.com>
2024-11-30 19:48:45 +0500

    Add files via upload

---
+++ 2-8-2024/app.js
@@ -0,0 +1,76 @@
+// let Data = "secret information"
+
+// class user{
+// constructor(name,email){
+//  this.name=name;
+//  this.email=email;   
+// }
+
+// viewData(){ 
+// console.log("data =", Data);
+// }                                                                                                                                                                       
+// };
+
+
+
+
+// class Admin extends user{
+
+
+// constructor(name,email){
+// super(name,email)
+
+// }
+
+// editdata(){
+// Data="some new value"
+// }
+
+
+// }
+
+// let student1 = new user("Moiz", "moiz@email.com"); 
+// let student2 = new user("hussain", "hussain@email.com");
+
+// let admin1 = new Admin("Hassan","Hassan@email.com")
+
+
+
+let a = 5;
+let b = 10;
+
+
+console.log("a=", a );
+console.log("a=", b);
+console.log("a+b=", a+b);
+try{
+    console.log("a+b=", a+c);
+}
+catch (err){
+console.log(err);
+}
+console.log("a+b=", a+b);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
