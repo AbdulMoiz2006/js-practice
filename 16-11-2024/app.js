df69926b45cd12b77392015bd1a512364d2ec28a
AbdulMoiz2006 <156921517+AbdulMoiz2006@users.noreply.github.com>
2024-11-30 19:45:23 +0500

    Add files via upload

---
+++ 16-11-2024/app.js
@@ -0,0 +1,165 @@
+// var username = prompt("Enter your name")
+// if(username=== "Ali"){
+
+// alert(`Hello,${username}! Welcome!`)
+// alert(`Hope you're having a great day,${username}!`)
+
+// console.log("Special access granted to Ali");
+
+// }else if(username ===""){
+
+//     alert("You didnt enter your name!")
+
+// }else{
+//     alert(`Hello,${username}!`)
+// }
+
+
+
+
+
+// var username = prompt("Enter your name")
+
+// if(username === "Ali"){
+
+//     alert(`Hello,${username}!Welcome!`)
+//     alert(`Hope you're having a great day,${username}!`)
+
+// }
+// else if(username === ""){
+
+
+
+//  alert("You didnt enter your name!")
+
+// }
+
+
+
+// else{
+//     alert(`Hello,${username}!`)
+// }
+
+
+
+
+// var age = prompt("Enter your Age")
+// if(age < 18 ){
+
+// alert("You are not eligible to vote.")
+
+
+// }else{
+
+
+//     alert("You are  eligible to vote.")
+
+// }
+
+
+
+// var age = prompt("Enter your Age")
+
+// if(isNaN(age)){
+
+//     alert("This is not a valid number!");
+
+// }else{
+
+//     alert("This is a valid number.");
+// }
+
+
+// var age = prompt("Enter your Age")
+
+// if(age === "" || isNaN(age)|| age < 0){
+
+//     alert("Please enter a valid age.");
+
+// }
+// else{
+// var yearTo100 = 100 - age
+
+// alert(`You are currently ${age} years old. In ${yearTo100} years, you will turn 100!`)
+    
+// }
+
+
+//Task 9 Chap#69 Object
+
+
+// var person={
+
+//     name:"Moiz",
+//     Age:18,
+//     BirthYear:2024 - 18
+
+
+// }
+
+// console.log(Object.values(person));
+
+
+
+// var age = ["18","19","20","21","22"]
+
+// for(var i=0 ; i<age.length ; i++){
+
+// console.log(age[i]);
+
+
+// }
+
+
+//Task 1
+
+
+// var num1 = parseFloat(prompt("Enter your first number"))
+// var num2 = parseFloat(prompt("Enter your second number"))
+
+// Sum = num1 + num2;
+
+// alert(`The sum of ${num1} and ${num2} is ${Sum}`)
+
+
+
+//Task 2
+
+// var currentPass = "12345"
+
+// var userPass = prompt("Enter your Password")
+
+// if(userPass===currentPass){
+//     alert("Your Password is Verify ")
+
+// }else{
+
+//     alert("Your Password is Incorrect!")
+// }
+
+
+
+// var userName = prompt("Enter your name")
+
+// function message(userName){
+
+// alert(`Hello ${userName}`)
+
+
+// }
+
+// message(userName)
+
+
+ var num1 = parseFloat(prompt("Enter your first number"))
+ var num2 = parseFloat(prompt("Enter your second number"))
+
+ function sum(num1,num2){
+
+
+return 
+
+}
+
+    
+var sum = num1+num2
